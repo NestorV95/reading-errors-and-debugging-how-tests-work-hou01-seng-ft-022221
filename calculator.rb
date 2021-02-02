2cd31e92c48de0bb364f3b2f122ea162ec5469d8
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-02 01:28:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,6 @@
 first_number = 2
 second_number = 6
-
-def sum = first_number + second_number
-end
-
-def difference = first_number - second_number
-end
-
-def product = first_number * second_number
-end
-
-def quotient = first_number / second_number
-end
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
