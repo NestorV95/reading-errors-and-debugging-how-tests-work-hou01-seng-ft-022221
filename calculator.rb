b8d1cdb21f5a460a3c1db9841cfa8bcf104f55d0
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-02 01:15:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,18 @@
 first_number = get_variable_from_file
 second_number = get_variable_from_file
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
+
+def sum
+first_number + second_number
+end
+
+def difference
+first_number - second_number
+end
+
+def product 
+  first_number * second_number
+end
+
+def quotient
+  first_number / second_number
+end
