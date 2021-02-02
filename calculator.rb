9938633cfde284ef27ea59c08c508d768894806e
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-02 01:07:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,4 @@ first_number = get_variable_from_file
 second_number = get_variable_from_file
 sum = first_number + second_number
 difference = first_number - second_number
-
+product = first_number * second_number
