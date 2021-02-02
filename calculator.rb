69d014eee033fb94dd9addbec463e322aed88d89
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-02 01:07:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,4 @@ second_number = get_variable_from_file
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
